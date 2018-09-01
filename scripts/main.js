97fd337d2e43fcd298489ee7af00e6b0ced12bba
Antony <metalization25@gmail.com>
2018-09-02 00:30:49 +0300

    Simple function and onClickListener example

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello fucking World BLYAT\'!';
+
+function multiply(num1, num2) {
+  var result = num1 * num2;
+  return result;
+}
+
+document.querySelector('img').onclick = function() {
+    alert('Fucking random picture clicked!');
+}
