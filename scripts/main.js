e336aa1a8ea2aa5fb8fe697b83c7635a278f9285
Antony <metalization25@gmail.com>
2018-09-02 01:08:41 +0300

    prompt example

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,38 @@
 var myHeading = document.querySelector('h1');
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var appendString = 'Mozilla is cool, but you\'re faggit, mr. ';
+
 myHeading.textContent = 'Hello fucking World BLYAT\'!';
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = appendString + storedName;
+}
 
 function multiply(num1, num2) {
   var result = num1 * num2;
   return result;
 }
 
-document.querySelector('img').onclick = function() {
-    alert('Fucking random picture clicked!');
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/random_pic.jpg') {
+    myImage.setAttribute('src','images/random_pic2.jpeg')
+  } else {
+    myImage.setAttribute('src','images/random_pic.jpg')
+  }
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your fuckin\' name, bitch!');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = appendString + myName;
 }
